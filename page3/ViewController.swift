bba3c75ac58ed3ecae624dd6c5340ddd92392007
Leon Cheng <nokia6102@gmail.com>
2018-09-17 21:42:39 +0800

    Initial Commit

---
+++ page3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  page3
+//
+//  Created by Leon Cheng on 2018/9/17.
+//  Copyright © 2018年 Leon Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
